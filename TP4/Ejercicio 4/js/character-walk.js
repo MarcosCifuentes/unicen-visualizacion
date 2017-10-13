8357cba83ee0afdb62c9ce3926436c654d4017b5
MarcosCifuentes <marcosnaza16@gmail.com>
2017-10-13 17:44:23 -0300

    ejercicio 4 funcionando

---
+++ TP4/Ejercicio 4/js/character-walk.js
@@ -0,0 +1,8 @@
+// addEventListener("mousedown", myFunction);
+//
+// function myFunction(e) {
+//   x=e.clientX;
+//   y=e.clientY;
+//   let personaje = document.getElementsByClassName('character');
+//   personaje.style.tranform=translate(x+"px",y+"px");
+// }
